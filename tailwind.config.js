3a38bd2ed17704011125dd9f929ce95e8da56b28
Santiago Botero <santiago.botero@kommit.co>
2022-05-17 17:42:47 -0500

    New title and some metatags

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,10 @@ module.exports = {
       PTSans: ['PT Sans', 'sans-serif'],
     },
     screens: {
-      smartphone: {max: "900px"},
-      laptop: {min: "901px", max: "1360px"},
-      desktop: {min: "1361px"},
-      computer:{min: "901px"},
+      smartphone: { max: "900px" },
+      laptop: { min: "901px", max: "1360px" },
+      desktop: { min: "1361px" },
+      computer: { min: "901px" },
     },
     colors: {
       primary: "#39FFC5",
