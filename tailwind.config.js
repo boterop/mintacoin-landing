58b1aaa5f944a0c612d9b59e638296bbdf19345f
Santiago Botero <santiago.botero@kommit.co>
2022-05-17 16:21:33 -0500

    Landing page v2 for mintacoin

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/public/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      PTSans: ['PT Sans', 'sans-serif'],
+    },
+    screens: {
+      smartphone: {max: "900px"},
+      laptop: {min: "901px", max: "1360px"},
+      desktop: {min: "1361px"},
+      computer:{min: "901px"},
+    },
+    colors: {
+      primary: "#39FFC5",
+      white: "#FFFFFF",
+      black: "#000000",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
